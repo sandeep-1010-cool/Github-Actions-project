bdad21b2560d9b39bc8813365647093722ff8c69
sandeep <vuyyalasandeep@gmail.com>
2025-07-26 10:24:43 +0530

    AMI and ec2 deploy

--- pulumi_project/index.ts
+++ pulumi_project/index.ts
@@ -4,15 +4,18 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 
+// Get the latest ami
+const ami = aws.ec2.getAmiOutput({
+    mostRecent: true,
+    owners: ["amazon"],
+    filters: [
+        { name: "name", values: ["amzn2-ami-hvm-*-x86_64-gp2"] },
+    ],
+});
+
 // Create a new ec2 instance
 const ec2Instance = new aws.ec2.Instance("my-ec2-instance", {
-    ami: aws.getAmi({
-        mostRecent: true,
-        owners: ["amazon"],
-        filters: [
-            { name: "name", values: ["amzn2-ami-hvm-*-x86_64-gp2"] },
-        ],
-    }).then(ami => ami.id),
+    ami: ami.id,
     instanceType: "t2.micro",
     tags: {
         Name: "my-ec2-instance",
