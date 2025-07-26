ca0f196662a876331daa4de0d6fa4763af3afefc
sandeep <vuyyalasandeep@gmail.com>
2025-07-26 10:09:34 +0530

    Remove redundant bucket ACL configuration from Pulumi S3 bucket setup to simplify resource management.

--- pulumi_project/index.ts
+++ pulumi_project/index.ts
@@ -7,11 +7,5 @@ const bucket = new aws.s3.Bucket("my-bucket", {
     bucket: pulumi.interpolate`my-unique-bucket-${pulumi.getStack()}-${pulumi.getProject()}`,
 });
 
-// Create bucket ACL resource (modern approach)
-const bucketAcl = new aws.s3.BucketAcl("my-bucket-acl", {
-    bucket: bucket.id,
-    acl: "private",
-});
-
 // Export the bucket name
 export const bucketName = bucket.id;
