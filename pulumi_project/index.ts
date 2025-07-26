0d670e68250f4e626dbe78d25e5d07d6d30496f1
sandeep <vuyyalasandeep@gmail.com>
2025-07-26 09:47:46 +0530

    Update Pulumi workflow to set AWS region dynamically and enhance S3 bucket creation with unique naming and ACL configuration.

--- pulumi_project/index.ts
+++ pulumi_project/index.ts
@@ -1,8 +1,15 @@
 import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
 
-// Create an S3 bucket
+// Create an S3 bucket with a unique name
 const bucket = new aws.s3.Bucket("my-bucket", {
-    bucket: "my-unique-bucket-name",
+    // Let Pulumi generate a unique name
+    bucket: pulumi.interpolate`my-unique-bucket-${pulumi.getStack()}-${pulumi.getProject()}`,
+});
+
+// Create bucket ACL resource (modern approach)
+const bucketAcl = new aws.s3.BucketAcl("my-bucket-acl", {
+    bucket: bucket.id,
     acl: "private",
 });
 
